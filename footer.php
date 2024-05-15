fcc0e8d23d0ec47190f2bd70e44d71078f97b072
NayChi\Nay Chi Shunn Lae <nay-chishunnlae@bit-gu.com>
2024-05-15 12:00:07 +0900

    initial upload

---
+++ footer.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains all content after the main content area and sidebar
+ *
+ * @package Poseidon
+ */
+
+?>
+<!-- FOOTER AREA -->
+<footer id="footer" data-aos="fade-up">
+
+	<div class="footer-newsletter">
+
+		<div class="container">
+			<h4>LINE公式アカウント</h4>
+			<p>EASYWORKER FIT.のお得な情報をお届けしますので、QRコードから登録してください。</p>
+			<div class="d-flex justify-content-center mt-3">
+				<img src="<?php bloginfo('template_url'); ?>/assets/images/LINE_Brand_icon.png" class="img-fluid me-3"><img src="<?php bloginfo('template_url'); ?>/assets/images/qr.png" class="img-fluid ms-3" /> 
+			</div>
+		</div>
+	</div>
+
+	<div class="footer-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 footer-contact">
+					<h3><img src="<?php bloginfo('template_url'); ?>/assets/images/alternate_logo.png" class="img-fluid"/></h3>
+					<p>
+						〒220‐0012<br>
+						神奈川県横浜市西区みなとみらい4-4-2<br>
+						横浜ブルーアベニュー5F
+					<div class="mt-4">
+						<strong>Phone:</strong>000-000-0000<br>
+						<strong>From:</strong> info@example.com<br>
+					</div>
+					</p>
+				</div>
+				<div class="col-lg-3 col-md-6 footer-links">
+					<h4>Our Services</h4>
+					<ul>
+						<li><i class="bx bx-chevron-right"></i> <a href="index.html">Home</a></li>
+						<li><i class="bx bx-chevron-right"></i> <a href="about.html">About us</a></li>
+						<li><i class="bx bx-chevron-right"></i> <a href="services.html">Services</a></li>
+						<li><i class="bx bx-chevron-right"></i> <a href="price.html">Price List</a></li>
+						<li><i class="bx bx-chevron-right"></i> <a href="staff.html">Gim Staff</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6 footer-links">
+					<h4>Our Services</h4>
+					<ul>
+						<li><i class="bx bx-chevron-right"></i> <a href="infomation.html">Infomation</a></li>
+						<li><i class="bx bx-chevron-right"></i> <a href="blog.html">Blog</a></li>
+						<li><i class="bx bx-chevron-right"></i> <a href="#">Contact</a></li>
+						<li><i class="bx bx-chevron-right"></i> <a href="sitemap.html">sitmap</a></li>
+						<li><i class="bx bx-chevron-right"></i> <a href="privacy.html">Privacy policy</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6 footer-links">
+					<h4>Our Social Networks</h4>
+					<div class="social mt-2">
+						<a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+						<a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+						<a href="#"><i class="fa-brands fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container footer-bottom clearfix">
+		<div class="copyright">
+			&copy; Copyright <strong><span>EASYWORKER FIT.</span></strong>. All Rights Reserved
+		</div>
+		<div class="credits">
+			<!-- All the links in the footer should remain intact. -->
+			<!-- You can delete the links only if you purchased the pro version. -->
+			<!-- Licensing information: https://bootstrapmade.com/license/ -->
+			<!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+			Produce by <a href="https://bootstrapmade.com/">DIGITAL WORKFORCE</a>
+		</div>
+	</div>
+</footer><!-- END FOOTER AREA -->
+
+<?php wp_footer(); ?>
+
+
---
+++ header.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Poseidon
+ */
+
+?>
+<!DOCTYPE html>
+<!-- <html <?php language_attributes(); ?>> -->
+
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>WEB01</title>
+	<meta content="" name="description">
+	<meta content="" name="keywords">
+
+	<!-- Favicons -->
+	<link href="assets/img/favicon.png" rel="icon">
+	<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Vendor CSS Files -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<!-- CSS FILES -->
+	<?php wp_head(); ?>
+
+
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-6dXLPV0m/dsWhg5p9sqR6sDjePbf+o9ALq4x7UMmVl45OVMm+kswT6ZUtZ2BMQ0S6u8A1o5O8r1lK/eAZWx7tg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	
+
+</head>
+
+<!-- HEADER START -->
+<header id="header" class="header fixed-top d-flex align-items-center">
+	<div class="container-fluid d-flex align-items-center justify-content-between">
+
+		<a href="index.html" class="logo d-flex align-items-center me-auto me-xl-0">
+			<h1 class="logo"><img src="<?php bloginfo('template_url'); ?>/assets/images/logo.png" class="img-fluid"/></h1>
+		</a>
+
+		<!-- NAV MENU AREA -->
+		<nav id="navmenu" class="navmenu">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="about.html">About Us</a></li>
+				<li><a href="services.html">Services</a></li>
+				<li><a href="price.html">Price List</a></li>
+				<li class="dropdown has-dropdown"><a href="staff.html"><span>Gim Staff</span> <i class="bi bi-chevron-down"></i></a>
+					<ul class="dd-box-shadow">
+						<li><a href="#">田中&nbsp;太郎</a></li>
+						<li><a href="#">佐藤&nbsp;健太</a></li>
+						<li><a href="#">高橋&nbsp;美咲</a></li>
+						<li><a href="#">伊藤&nbsp;真理</a></li>
+					</ul>
+				</li>
+				<li><a href="faq.html">FAQ</a></li>
+				<li><a href="infomation.html">Infomation</a></li>
+				<li><a href="blog.html">Blog</a></li>
+				<li><a href="#">Contact</a></li>
+				<li class="carender"><a href="#"><i class="bi bi-calendar-check-fill"></i>&nbsp;BOOKING CARENDAR</a></li>
+			</ul>
+
+			<i class="mobile-nav-toggle bi bi-list"></i>
+		</nav><!-- END NAV MENU AREA -->
+
+	</div>
+</header><!-- END HEADER -->
