66b196ab2d7d257ad41d74a640c0964bb29f3cd8
eiphyu99 <159862906+eiphyu99@users.noreply.github.com>
2024-05-17 12:07:14 +0900

    aboutページ作成

---
+++ about.php
@@ -0,0 +1,215 @@
+<?php
+/*
+Template Name: About Page Template
+*/
+get_header();
+?>
+<section id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<!-- PAN AREA -->
+		<nav class="pan" data-aos="fade-up" data-aos-delay="100">
+			<div class="container">
+				<ol>
+					<li><a href="index.html">Home</a></li>
+					<li class="current">About Us</li>
+				</ol>
+			</div>
+		</nav><!-- END PAN AREA -->
+
+
+		<!-- ABOUT US AREA -->
+		<section class="about">
+
+			<!--  SECTION TITLE -->
+			<div class="container section-title" data-aos="fade-up" data-aos-delay="300">
+				<h2 class="mb-3">About Us</h2>
+				<h3 class="mb-4">EASYWORKER FITについて</h3>
+				<p>あなたにに合わせたトレーニングで、安全に楽しく、健康的な身体づくりを目指しましょう！</p>
+			</div><!-- END SECTION TITLE -->
+
+			<div class="container">
+				<div class="row align-items-xl-center gy-5">
+
+					<div class="col-12 content">
+
+						<div data-aos="fade-up" data-aos-delay="400">
+							<p class="color01 mb-1">Personal training committed to results</p>
+							<h1>結果にコミットする、パーソナルトレーニング</h1>
+							<p class="mt-3 mb-3 lh-lg">EASYWORKER FITは、健康とフィットネスを追求する個々のニーズに焦点を当てた、革新的で専門的なトレーニングを提供するパーソナルジムです。私たちは2020年に設立され、それ以来、クライアントが健康的で活力に満ちた生活を送るために最適なサポートを提供してきました。ジム内では、最新のトレーニング機器と質の高いトレーナーチームが、クライアントの目標達成をサポートしています。</p>
+						</div>
+
+						<div class="row py-3" >
+							<div class="col-12 col-md-4 mb-3 mb-md-0" data-aos="fade-up" data-aos-delay="500">
+								<img src="<?php bloginfo('template_url'); ?>/assets/images/16.jpg" class="img-fluid"/> 
+							</div>
+							<div class="col-12 col-md-4 mb-3 mb-md-0" data-aos="fade-up" data-aos-delay="600">
+								<img src="<?php bloginfo('template_url'); ?>/assets/images/17.jpg" class="img-fluid"/>
+							</div>
+							<div class="col-12 col-md-4" data-aos="fade-up" data-aos-delay="700">
+								<img src="<?php bloginfo('template_url'); ?>/assets/images/18.jpg" class="img-fluid"/>
+							</div>
+						</div>
+
+						<div data-aos="fade-up" data-aos-delay="800">
+							<p class="color01 mt-4 mb-1">Build a healthy lifestyle with sustainable, fun, and effective training</p>
+							<h1>持続可能で楽しく、効果的なトレーニングで健康なライフスタイルを築く</h1>
+							<p class="mt-3 mb-3 lh-lg">EASYWORKER FITのビジョンは、個々のクライアントが健康なライフスタイルを築く上で、持続可能で楽しく、効果的なトレーニングの提供を通じて積極的な影響を与えることです。FitLife パーソナルジムでは、トレーニングは単なる身体の変革だけでなく、全体的なウェルビーイングの向上を促進する手段と捉えています。私たちはクライアントが自分自身の限界を挑戦し、その成果を喜びと自信に変えるお手伝いをすることを誇りにしています。</p>
+
+							<p class="mt-3 mb-3 lh-lg">EASYWORKER FITは、専門的な知識、個別のアプローチ、そして温かいコミュニティの中で、クライアントが挑戦し続け、持続可能な変化を実現できる場所です。FitLife パーソナルジムでは、あなたの個々の目標に合わせて調整されたトレーニング体験を提供し、健康とフィットネスの旅を共に歩んでいくことを楽しみにしています。</p>
+						</div>
+					</div>
+
+				</div>
+				<div class="text-center" data-aos="zoom-in" data-aos-delay="900" >
+					<p class="mt-5"><a href="#" class="btn_01"><i class="bi bi-calendar-check-fill"></i>&nbsp;BOOKING CARENDAR</a></p>
+				</div>
+			</div>
+		</section><!-- END ABOUT US AREA -->
+
+
+		<!-- VOICE AREA -->
+		<section class="voice">
+
+			<!--  SECTION TITLE -->
+			<div class="container section-title" data-aos="fade-up">
+				<h2 class="mb-3">Voice</h2>
+				<h3 class="mb-4">お客様の声</h3>
+				<p>パーソナルトレーニングにおいて、数多くのクライアントから寄せられた声をご紹介します。</p>
+			</div><!-- END SECTION TITLE -->
+
+			<div class="container">
+				<div class="row">
+
+					<div class="col-12 col-md-6 mb-5 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+						<div class="d-flex flex-column">
+							<div class="text-start mb-2"><img src="<?php bloginfo('template_url'); ?>/assets/images/19.jpg" /></div>
+							<div class="balloon-002 h-100">
+								<div class="text-container">
+									<p class="heading01 mb-2">30歳&nbsp;女性&nbsp;「健康なライフスタイルの発見」</p>
+									<p>トレーニングを通じて、健康的な食習慣や効果的なエクササイズの重要性を学びました。今では自分の身体と心に対する新しい理解を持ち、健康的なライフスタイルを楽しんでいます。</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 mb-5 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+						<div class="d-flex flex-column">
+							<div class="text-start mb-2"><img src="<?php bloginfo('template_url'); ?>/assets/images/19.jpg" /></div>
+							<div class="balloon-002 h-100">
+								<div class="text-container">
+									<p class="heading01 mb-2">50歳&nbsp;女性&nbsp;「年齢を感じさせない体力向上」</p>
+									<p>年齢を理由に諦めかけていましたが、トレーナーの助言とサポートで体力向上に成功。今、若い頃以上に元気でいられることに感謝しています。</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 mb-5 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+						<div class="d-flex flex-column">
+							<div class="text-start mb-2"><img src="<?php bloginfo('template_url'); ?>/assets/images/20.jpg" /></div>
+							<div class="balloon-002 h-100">
+								<div class="text-container">
+									<p class="heading01 mb-2">30歳&nbsp;男性&nbsp;「ストレス解消と心の健康」</p>
+									<p>ストレスフルな仕事に悩まされていたが、トレーニングを通じて心と体のバランスを取り戻しました。穏やかな日常に感謝しています。</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 mb-5 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+						<div class="d-flex flex-column">
+							<div class="text-start mb-2"><img src="<?php bloginfo('template_url'); ?>/assets/images/20.jpg" /></div>
+							<div class="balloon-002 h-100">
+								<div class="text-container">
+									<p class="heading01 mb-2">20歳&nbsp;男性&nbsp;「リモートトレーニングの便利さ」</p>
+									<p>仕事の都合でジムに通うのが難しかった私にとって、リモートトレーニングは救世主でした。効果的なプログラムとフレキシブルなスケジュールで、理想的な結果を得られました。</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 mb-5 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="500">
+						<div class="d-flex flex-column">
+							<div class="text-start mb-2"><img src="<?php bloginfo('template_url'); ?>/assets/images/20.jpg" /></div>
+							<div class="balloon-002 h-100">
+								<div class="text-container">
+									<p class="heading01 mb-2">30歳&nbsp;女性&nbsp;「忙しいママのための自己ケア」</p>
+									<p>子育てに追われる毎日でしたが、パーソナルトレーニングで自分自身へのケアを再発見。エネルギッシュで健康なママに変身できました。</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 mb-5 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="600">
+						<div class="d-flex flex-column">
+							<div class="text-start mb-2"><img src="<?php bloginfo('template_url'); ?>/assets/images/20.jpg" /></div>
+							<div class="balloon-002 h-100">
+								<div class="text-container">
+									<p class="heading01 mb-2">40歳&nbsp;男性&nbsp;「エネルギッシュな自分」</p>
+									<p>私は40代の男性で、仕事の忙しさから健康を犠牲にしていました。しかし、このパーソナルトレーニングで生活が一変し、エネルギッシュな自分に出会えました。トレーナーの専念したサポートに感謝しています。</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section><!-- END VOICE AREA -->
+
+
+		<!-- CONTACT AREA -->
+		<section id="contact" class="contact">
+			<div class="container" data-aos="fade-up">
+
+				<!--  SECTION TITLE -->
+				<div class="container section-title" data-aos="fade-up">
+				<h2 class="mb-3">CONTACT</h2>
+				<h3 class="mb-4">お問い合わせ</h3>
+				<p>各種お問い合わせ方法や予約カレンダーのご案内になります。</p>
+				</div><!-- END SECTION TITLE -->
+
+				<div class="row">
+
+					<div class="col-lg-5 d-flex align-items-stretch">
+						<div class="info" data-aos="fade-up" data-aos-delay="200">
+							<div class="address">
+								<i class="bi bi-geo-alt"></i>
+								<h4>Address:</h4>
+								<p>〒220‐0012<br>
+								神奈川県横浜市西区みなとみらい4-4-2<br>
+								横浜ブルーアベニュー5F</p>
+							</div>
+
+							<div class="phone">
+								<i class="bi bi-phone"></i>
+								<h4>Phone:</h4>
+								<p>000-000-0000</p>
+							</div>
+
+							<div class="email">
+								<i class="bi bi-envelope"></i>
+								<h4>Contact From:</h4>
+								<p>info@example.com</p>
+							</div>
+
+							<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5158.761664648334!2d139.62605319712668!3d35.4593487695378!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60185dde212a6c31%3A0x85721d07bc5c0f96!2z5qCq5byP5Lya56S-44OH44K444K_44Or44Ov44O844Kv44OV44Kp44O844K5!5e0!3m2!1sja!2sjp!4v1709861403057!5m2!1sja!2sjp" width="100%" height="290px;" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+						</div>
+
+					</div>
+
+					<div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+						<div class="carender text-center">
+							<h4>Booking Carender</h4>
+							<img src="<?php bloginfo('template_url'); ?>/assets/images/images02.png" class="img-fluid"/> 
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</section><!-- END CONTACT AREA -->
+
+	</main><!-- #main -->
+</section><!-- #primary -->
+<?php get_footer(); ?>
