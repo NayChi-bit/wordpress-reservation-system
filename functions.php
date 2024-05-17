40f649eaadda1f7d945d416059847d6683159712
NayChi\Nay Chi Shunn Lae <nay-chishunnlae@bit-gu.com>
2024-05-17 14:19:51 +0900

    コメント更新

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
       //Favicons
 
 
-      // Vendor CSS Files 
+      // Vendor CSS ファイル
       wp_register_style('bootstrap_css', get_template_directory_uri() . '/assets/vendor/bootstrap/css/bootstrap.min.css', false,'0.1','all');
       wp_enqueue_style( 'bootstrap_css');
       
@@ -22,12 +22,14 @@
       wp_register_style('aos_css', get_template_directory_uri() . '/assets/vendor/aos/aos.css', false,'0.1','all');
       wp_enqueue_style( 'aos_css');
 
-      // CSS FILES 
+      // CSS ファイル
       wp_register_style('style', get_template_directory_uri() . '/style.css', false,'0.1','all');
       wp_enqueue_style( 'style');
       
       wp_register_style('index', get_template_directory_uri() . '/css/index.css', false,'0.1','all');
-      wp_enqueue_style( 'index');
+      if (!is_page()) {
+         wp_enqueue_style( 'index');
+      }
 
       wp_register_style('services', get_template_directory_uri() . '/css/services.css', false,'0.1','all');
       if (is_page('services')) {
@@ -44,7 +46,7 @@
 
    function add_script()
    {
-      // Vendor JS Files
+      // Vendor JS ファイル
       wp_register_script('bootstrap_js', get_template_directory_uri() . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', array ( 'jquery' ), 0.1, true);
       wp_enqueue_script( 'bootstrap_js');
       
@@ -63,7 +65,7 @@
       wp_register_script('aos_js', get_template_directory_uri() . '/assets/vendor/aos/aos.js', array ( 'jquery' ), 0.1, true);
       wp_enqueue_script( 'aos_js');
 
-      // Template Main JS File
+      // Main JS ファイル
       wp_register_script('js-script', get_template_directory_uri() . '/assets/js/main.js', array ( 'jquery' ), 0.1, true);
       wp_enqueue_script( 'js-script');
       
